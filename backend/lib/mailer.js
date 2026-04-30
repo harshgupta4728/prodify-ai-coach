9ca68abd71d7c91981e58b921a5efc0cede8d5ef
Harsh Gupta <harsh.gupta@busy.in>
2026-04-30 18:18:47 +0530

    Update email app password

--- backend/lib/mailer.js
+++ backend/lib/mailer.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER || 'harshgupta4728@gmail.com',
-    pass: process.env.EMAIL_PASS || 'pfjh ueik nuao koty'
+    pass: process.env.EMAIL_PASS || 'jjaf bdse htdj wkjz'
   }
 });
 
